eb418bc3b00d796bf51384091f2c7f014a3dcbef
trevorlaing <trevor.laing@gmail.com>
2025-05-25 17:47:40 -0400

    Corrected syntax in for statements.

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,27 +1,28 @@
-def bubble_sort(List):  
-    for i in range(len(List):
+def bubble_sort(List):
+    for i in range(len(List)):  # Corrected: Added closing parenthesis
         swapped = False
-        
+
         for j in range(0, len(List) - i - 1):
             if List[j] > List[j + 1]:
                 List[j], List[j + 1] = List[j + 1], List[j]  # Swap
                 swapped = True
-                
+
         if not swapped:
             break
-    
+
     return List
+
 #reverse
-def bubble_sort_desc(List):  
-    for i in range(len(List):
+def bubble_sort_desc(List):
+    for i in range(len(List)):  # Corrected: Added closing parenthesis
         swapped = False
-        
+
         for j in range(0, len(List) - i - 1):
             if List[j] < List[j + 1]:
                 List[j], List[j + 1] = List[j + 1], List[j]  # Swap
                 swapped = True
-                
+
         if not swapped:
             break
-    
+
     return List
