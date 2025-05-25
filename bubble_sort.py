b3f972b2e80e23845f7eb738105d212db97147bc
trevorlaing <trevor.laing@gmail.com>
2025-05-25 17:41:19 -0400

    This version has a reverse funtion added

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,27 @@
-def bubble_sort(score):  
-    for i in range(len(score)):
+def bubble_sort(List):  
+    for i in range(len(List):
         swapped = False
         
-        for j in range(0, len(score) - i - 1):
-            if score[j] > score[j + 1]:
-                score[j], score[j + 1] = score[j + 1], score[j]  # Swap
+        for j in range(0, len(List) - i - 1):
+            if List[j] > List[j + 1]:
+                List[j], List[j + 1] = List[j + 1], List[j]  # Swap
                 swapped = True
                 
         if not swapped:
             break
     
-    return score
+    return List
+#reverse
+def bubble_sort_desc(List):  
+    for i in range(len(List):
+        swapped = False
+        
+        for j in range(0, len(List) - i - 1):
+            if List[j] < List[j + 1]:
+                List[j], List[j + 1] = List[j + 1], List[j]  # Swap
+                swapped = True
+                
+        if not swapped:
+            break
+    
+    return List
